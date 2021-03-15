6f9a10ca0c273e4566d30ab83ea1c806ea50963f
Sean Grant <sgrant177@yahoo.com>
2021-03-15 16:17:25 -0400

    fix template

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://root:root@cluster0.byuzb.mongodb.net/GrantsCustomCreations?retryWrites=true&w=majority",
-    secretOrKey: "sgQ^h{>-3&`MW&P&Ue;P9j&Rqc/\c9MekJpn.pShVsjj<Z~LXfG"
-  };
